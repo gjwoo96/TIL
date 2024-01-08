a2fdb21f2bf516ac0b23530555a66a4db877d8c8
gjwoo96 <gjwoo96@naver.com>
2024-01-09 01:26:41 +0900

    28강 정리

--- Spring/library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ Spring/library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateReuqest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,18 +18,21 @@ public class UserServiceV2 {
 
     public UserServiceV2(UserRepository userRepository) {
         this.userRepository = userRepository;
+
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest request){
        User u =  userRepository.save(new User(request.getName(),request.getAge()));
+       throw new IllegalArgumentException();
     }
-
+    @Transactional(readOnly = true)
     public List<UserResponse>getUsers(){
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
-
+    @Transactional
     public void updateUser(UserUpdateReuqest request){
         // findById는 select * from user where id = ? 라는 쿼리를 실행하는것과 같고
         // 결과로는 Optional<User>형태로 나온다.(OptionalJDK8에 등장했다)
@@ -38,9 +42,9 @@ public class UserServiceV2 {
 
         //Update 쿼리 부분
         user.updateName(request.getName());
-        userRepository.save(user);
+        //userRepository.save(user);
     }
-
+    @Transactional
     public void deleteUser(String name){
         // 이름을 조건으로 조회방법
         // SELECT * FROM user WHERE name = ?
